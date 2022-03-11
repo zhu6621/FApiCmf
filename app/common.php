55a4bcc196532af440e33ea5deaf480595fac0aa
zhu6621 <441681185@qq.com>
2022-03-11 11:13:36 +0800

    身份证号码判断
    身份证号码判断

--- app/common.php
+++ app/common.php
@@ -1301,5 +1301,50 @@ if (!function_exists('get_client_ip_extend')) {
 }
 
 
+/**
+ * 判断是否为合法的身份证号码
+ * @param $mobile
+ * @return int
+ */
+
+if (!function_exists('isCreditNo')) {
+    function isCreditNo($vStr)
+    {
+        $vCity = array(
+            '11', '12', '13', '14', '15', '21', '22',
+            '23', '31', '32', '33', '34', '35', '36',
+            '37', '41', '42', '43', '44', '45', '46',
+            '50', '51', '52', '53', '54', '61', '62',
+            '63', '64', '65', '71', '81', '82', '91'
+        );
+        if (!preg_match('/^([\d]{17}[xX\d]|[\d]{15})$/', $vStr)) return false;
+        if (!in_array(substr($vStr, 0, 2), $vCity)) return false;
+        $vStr    = preg_replace('/[xX]$/i', 'a', $vStr);
+        $vLength = strlen($vStr);
+        if ($vLength == 18) {
+            $vBirthday = substr($vStr, 6, 4) . '-' . substr($vStr, 10, 2) . '-' . substr($vStr, 12, 2);
+        } else {
+            return false;
+            $vBirthday = '19' . substr($vStr, 6, 2) . '-' . substr($vStr, 8, 2) . '-' . substr($vStr, 10, 2);
+        }
+        $vBirthday_one = substr($vBirthday, 0, 1);
+        if ($vBirthday_one > 2 || $vBirthday_one == 0) {
+            return false;
+        }
+        if (date('Y-m-d', strtotime($vBirthday)) != $vBirthday) return false;
+        if ($vLength == 18) {
+            $vSum = 0;
+            for ($i = 17; $i >= 0; $i--) {
+                $vSubStr = substr($vStr, 17 - $i, 1);
+                $vSum    += (pow(2, $i) % 11) * (($vSubStr == 'a') ? 10 : intval($vSubStr, 11));
+            }
+            if ($vSum % 11 != 1) return false;
+        }
+        return true;
+    }
+}
+
+
+
 
 

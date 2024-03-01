9d514367ce58dc1980993f1709469799515da707
zhu6621 <441681185@qq.com>
2024-03-01 11:03:18 +0800

    Create Game2.php
    增加Game2

---
+++ app/api/controller/Game2.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace app\api\controller;
+
+
+use app\api\controller\common\Base;
+use app\service\game\GameService;
+use think\facade\Db;
+use think\facade\Request;
+
+class Game extends Base
+{
+    public $public_action = [];
+
+
+    /**
+     * 游戏广告配置
+     * Created by PhpStorm.
+     * @return null
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     * User: 小朱
+     * Date: 2023/3/22
+     * Time: 20:10
+     */
+    public function taskConfig()
+    {
+
+        $result = GameService::taskConfig();
+        return ajaxReturn($result);
+
+    }
+}
